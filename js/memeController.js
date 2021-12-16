cd4f9b72d415c88897c2b94e5b264411ade13a37
Danny Starobin <dannystar1@gmail.com>
2021-12-16 20:01:05 +0200

    commit 1

--- js/memeController.js
+++ js/memeController.js
@@ -1,11 +1,7 @@
 var gCanvas;
 var gCtx;
 
-function init() {
-    gCanvas = document.querySelector('#canvas');
-    gCtx = gCanvas.getContext('2d');
 
-   }
 
 function drawImgFromlocal(x) {
     var img = new Image()
@@ -28,4 +24,25 @@ function downloadCanvas(elLink) {
     const data = gCanvas.toDataURL()
     elLink.href = data
     elLink.download = 'my-img.jpg'
+}
+
+function drawRect(x, y) {
+    gCtx.rect(x, y,500, 50);
+    // gCtx.fillStyle = 'blue';
+    // gCtx.fillRect(x, y, 150, 150);
+    gCtx.strokeStyle = '#1b1b1b';
+    gCtx.stroke();
+}
+
+function drawText(txt, x, y) {
+    // gCtx.font = '48px serif';
+    // gCtx.fillText(txt, x, y);
+    gCtx.textBaseline = 'middle';
+    gCtx.textAlign = 'center';
+    // gCtx.lineWidth = 2;
+    // gCtx.strokeStyle = 'red';
+    gCtx.font = '30px monospace';
+    gCtx.fillStyle = 'black';
+    gCtx.fillText(txt, x, y);
+    // gCtx.strokeText(txt, x, y);
 }
